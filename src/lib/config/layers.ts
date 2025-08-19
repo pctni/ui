6f469f2b38c5d5a69716b80b9f5a415250f34a96
roblinlovelace <rob00x@gmail.com>
2025-08-19 12:09:12 +0100

    chore(coherent-network): simplify to single orange color and legend

--- src/lib/config/layers.ts
+++ src/lib/config/layers.ts
@@ -109,13 +109,7 @@ export const LAYERS: Record<string, LayerConfig> = {
 		sourceLayer: 'corenet_network_ni_2025-06',
 		type: 'line',
 		paint: {
-			'line-color': [
-				'step',
-				['to-number', ['get', 'bicycle_godutch'], 0],
-				'#ffbf00',
-				1000,
-				'#de3163'
-			],
+			'line-color': '#ffbf00',
 			'line-width': ['interpolate', ['linear'], ['zoom'], 8, 3, 12, 5, 16, 8]
 		}
 	},
